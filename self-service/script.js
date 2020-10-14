5b4f3d0536e184d64908e567c1f807a4cb376335
lfchaves18 <lfcc18@gmail.com>
2020-10-14 00:12:02 -0300

    ajuste botões

--- self-service/script.js
+++ self-service/script.js
@@ -1,25 +1,36 @@
-function CalcComida() {
 
-    var qte = document.getElementById('num');
-    qte = 0;
-  
-    document.getElementById('menos').click(function() {
-      qte--;
-    });
-    document.getElementById('mais').click(function() {
-      qte++;
-    });
-  }
+numero = 0;
 
-  function CalcBebida() {
+function less() {
+  numero--;
+  setValue(numero);
+}
 
-    var bebida = document.getElementById('num');
-    bebida = 0;
-  
-    document.getElementById('menos').click(function() {
-      bebida--;
-    });
-    document.getElementById('mais').click(function() {
-      bebida++;
-    });
-  }
+function more() {
+  numero++;
+  setValue(numero);
+}
+
+function setValue(value) {
+  document.getElementById('num').value = value;
+}
+
+setValue(numero);
+
+numeroBebida = 0;
+
+function lessBebida() {
+    numeroBebida--;
+  setValue(numernumeroBebidao);
+}
+
+function moreBebida() {
+    numeroBebida++;
+  setValue(numeroBebida);
+}
+
+function setValue(value) {
+  document.getElementById('numBebida').value = value;
+}
+
+setValue(numeroBebida); 
